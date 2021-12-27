41ff741481f3cff0be9bfa6fa8bcc21b6aecc13d
Dlunhappy226 <76965487+Dlunhappy226@users.noreply.github.com>
2021-12-26 21:22:56 -0800

    1.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="nettool",
-    version="1.0",
+    version="1.1",
     packages=["nettool"],
     url="https://github.com/Dlunhappy226/nettool",
     license="MIT",
     author="Dlunhappy226",
     author_email="dlunhappy226@gmail.com",
     description="Nettool",
-    download_url= "https://github.com/Dlunhappy226/nettool/archive/1.0.tar.gz",
+    download_url= "https://github.com/Dlunhappy226/nettool/archive/1.1.tar.gz",
     entry_points = {
         "console_scripts": [
             "nettool = nettool.nettool:main"
